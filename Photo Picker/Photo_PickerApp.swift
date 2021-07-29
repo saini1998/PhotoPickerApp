c2bc19c7ab4436952d10d8cdf22d4dbac4c82f13
saini1998 <sainiaaryaman1998@gmail.com>
2021-07-29 19:16:03 +0100

    Initial Commit

---
+++ Photo Picker/Photo_PickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Photo_PickerApp.swift
+//  Photo Picker
+//
+//  Created by Aaryaman Saini on 29/07/21.
+//
+
+import SwiftUI
+
+@main
+struct Photo_PickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
